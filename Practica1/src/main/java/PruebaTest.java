0d811f1ca767a3c45b5f38560f733cd97d475ec0
valentina Andrade <andradevalentina96@gmail.com>
2022-02-01 20:25:06 +0100

    Practica1

---
+++ Practica1/src/main/java/PruebaTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaTest {
+
+	@Test
+	public void test() {
+	
+		
+		
+		
+		
+	}
+
+}
---
+++ Practica1/src/main/java/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+	public static void main(String[] args) {
+		
+	Persona p=new Persona();
+	p.setEdad(17);
+	p.setAltura(170);
+	p.setPeso(60);
+	p.setNombre("Manolo");
+	}
+	
+	
+	
+	
+}
